3c133439041902f0d93c2fd01b3ccc195823fbe1
n-rdy <giuseppegelardi@icloud.com>
2021-11-09 11:25:57 -0600

    add resume page, import into app, add route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 
 import Layout from './components/Layout'
 import Home from './pages/Home'
+import Resume from './pages/Resume'
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
     <Layout />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/resume' element={<Resume />} />
       </Routes>
     </>
   )
