e51e52aeea3788f5a987a846bbba89fa71b5e2c0
n-rdy <giuseppegelardi@icloud.com>
2021-11-09 11:40:29 -0600

    add appbar in layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
 
 export default function Layout() {
   return (
     <div>
+      <AppBar>
+        <Toolbar>
+          <Typography>
+            Toolbar
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <h1>Layout component</h1>
     </div>
   )
